2c2a11dbc44a823599ad5a64f0ea11432be03660
AndresRojo12 <andresrojo20013@gmail.com>
2025-01-04 15:47:17 -0500

    se actualiza el controlador de orderProduct

--- src/users/controllers/order-product.controller.ts
+++ src/users/controllers/order-product.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
-import { CreateOrderProductDto } from '../dtos/order-product.dto';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { CreateOrderProductDto, UpdateOrderProductDto } from '../dtos/order-product.dto';
 import { OrderProductService } from '../services/order-product.service';
 
 @Controller('order-product')
@@ -19,5 +19,15 @@ export class OrderProductController {
   @Post()
   create(@Body() data: CreateOrderProductDto){
     return this.orderProductService.create(data);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() changes: UpdateOrderProductDto){
+    return this.orderProductService.update(id,changes);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id:number){
+    return this.orderProductService.remove(id);
   }
 }
